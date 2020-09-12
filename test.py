94467b1ad79472a3aba665c51208660cedcda855
Rishav Bhagat <rishavb123@gmail.com>
2020-09-12 14:58:25 -0400

    testing around witht the data

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 from preprocessing import load_data
+from transforms import *
 
 data = load_data('./data/Land_and_Ocean_complete.txt')
-print(data[:3])
+
+means = np.mean(data.get_data_matrix(), axis=1)
+
+t = composite(shift(2), derivative, rec_remove_outliers)
+d = t(means)
+print(np.mean(d))
+
+plt.plot(d)
+# plt.plot(means)
+plt.show()
