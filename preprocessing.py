dc955a0d2298480bccef3e3c450165a1cd71da1b
Rishav Bhagat <rishavb123@gmail.com>
2020-09-12 14:58:00 -0400

    created loading data

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def load_data(fname):
     f = open(fname, 'r')
     lines = f.readlines()
@@ -5,19 +7,53 @@ def load_data(fname):
     lines = filter(lambda l: l[0] != '%', lines)
     lines = [l[:-1].split(" ") for l in lines][1:]
     lines = [[l for l in line if l != ''] for line in lines]
-    return lines
+
+    new_lines = []
+    for line in lines:
+        if line == []: break
+        new_lines.append(line)
+    lines = new_lines
+
+    data = None
+    uncertainty = None
+
+    base_year = 0
+
+    for line in lines:
+        year, month, data_point, uncertainty_point = convert_to_data_point(line)
+        if base_year == 0:
+            base_year = year
+            data = np.zeros((2021 - base_year, 12))
+            uncertainty = np.zeros_like(data)
+        shifted_year = year - base_year
+        shifted_month = month - 1
+        data[shifted_year][shifted_month] = data_point
+        uncertainty[shifted_year][shifted_month] = uncertainty_point
+
+    return DataWrapper(data, uncertainty, base_year)
 
 def convert_to_data_point(line):
     year = int(line[0])
     month = int(line[1])
-    data_point = DataPoint(float(line[2]), float(line[3]))
-    return year, month, data_point
+    data_point = float(line[2])
+    uncertainty_point = float(line[3])
+    return year, month, data_point, uncertainty_point
 
-class DataPoint:
+class DataWrapper:
 
-    # 95% of samples lie in data +- uncertainty
-    def __init__(self, data, uncertainty):
-        super().__init__()
+    def __init__(self, data, uncertainty, base_year):
         self.data = data
         self.uncertainty = uncertainty
+        self.base_year = base_year
 
+    def get_data_matrix(self):
+        return self.data
+
+    def get_uncertainty_matrix(self):
+        return self.uncertainty
+
+    def get(self, year, month):
+        return self.data[year - self.base_year][month - 1]
+
+    def get_uncertainty(self, year, month):
+        return self.uncertainty[year - self.base_year][month - 1]
